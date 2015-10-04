fc234c324b4c1463609cbd2161731ebe3b1bc743
Frazer McLean <frazer@frazermclean.co.uk>
2015-10-04 17:23:48 +0200

    Check dir exists

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,7 +5,11 @@ set -x
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew install findutils
-    gfind $HOME/Library/Caches/pip -type f -name '*.whl'  
+    if [ -d "$HOME/Library/Caches/pip" ]; then
+        gfind $HOME/Library/Caches/pip -type f -name '*.whl'  
+    fi
 else
-    find $HOME/.cache/pip -type f -name '*.whl'  
+    if [ -d "$HOME/.cache/pip" ]; then
+        find $HOME/.cache/pip -type f -name '*.whl'  
+    fi
 fi
