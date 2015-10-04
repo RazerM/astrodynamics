7b3c548889ba4d3c9058ee37c46d1f2d101ffa53
Frazer McLean <frazer@frazermclean.co.uk>
2015-10-04 17:34:50 +0200

    Use codecov command

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -6,6 +6,5 @@ set -x
 NO_COVERAGE_TOXENVS=(py32 py2flake8 py3flake8)
 if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ "${TOXENV}" ]]; then
     source ~/.venv/bin/activate
-    wget https://codecov.io/bash -O codecov.sh
-    bash codecov.sh -e TRAVIS_OS_NAME,TOXENV
+    codecov --env TRAVIS_OS_NAME TOXENV
 fi
